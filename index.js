975249a6512476300cea6d26dca9ea349a0b305d
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-03-25 04:49:59 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(cors({
   
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://blogingwebsite.vercel.app/"); // Replace * with your frontend URL for security
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 //Student Routes
 app.use('/blog',blogRoutes)
