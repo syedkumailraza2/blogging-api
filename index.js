7a7046132e842d8b6644dd8a2c3315a5ea817420
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-03-25 04:40:35 +0530

    cors added

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 8000
-app.use(cors());
+app.use(cors({
+    origin: '*', // Or specify your frontend URL e.g. 'https://yourfrontend.com'
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
