9e334a83c21a464d6bcfc17aa143d09b7ad46d8b
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-03-25 04:41:44 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 8000
 app.use(cors({
-    origin: '*', // Or specify your frontend URL e.g. 'https://yourfrontend.com'
+    origin: 'https://blogingwebsite.vercel.app/', // Or specify your frontend URL e.g. 'https://yourfrontend.com'
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }));
